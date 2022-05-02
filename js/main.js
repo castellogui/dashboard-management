540e1c8253d83c9c42af2b8b25fa111c231424c9
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-05-02 13:38:28 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+(function($) {
+
+    "use strict";
+
+    $(".toggle-password").click(function() {
+
+        $(this).toggleClass("fa-eye fa-eye-slash");
+        var input = $($(this).attr("toggle"));
+        if (input.attr("type") == "password") {
+            input.attr("type", "text");
+        } else {
+            input.attr("type", "password");
+        }
+    });
+
+})(jQuery);
+
+
+const validarUsuario = form => {
+    let user = JSON.parse(localStorage.getItem('user'))
+    if (user.user == document.getElementById('user-field').value && user.password == document.getElementById('password-field').value) {
+        console.log('Login bem sucedido!')
+        return true;
+    } else {
+        console.log('Erro no login')
+        exibirMensagem();
+        return false
+    }
+}
+
+const exibirMensagem = () => {
+    document.getElementById('error-message').style = 'display: block;'
+
+}
+
+
+const criarUsuarioTest = () => {
+    let user = { user: 'guilhermecastello', password: '123' }
+    localStorage.setItem('user', JSON.stringify(user))
+}
+
+criarUsuarioTest();
