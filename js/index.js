0eafa32b12c01a2f1586eddaf712e6da2289a494
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-05-03 22:22:03 -0300

    Ajustado método validar usuário para localstorage

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
 
     "use strict";
 
-    $(".toggle-password").click(function() {
+    $(".toggle-password").click(function () {
 
         $(this).toggleClass("fa-eye fa-eye-slash");
         var input = $($(this).attr("toggle"));
@@ -16,31 +16,60 @@
 })(jQuery);
 
 
+const buscarUsuarios = () => {
+    let users = JSON.parse(localStorage.getItem('users'));
+    return users;
+}
+
 const validarUsuario = form => {
-    let user = JSON.parse(localStorage.getItem('user'))
-    if (user.user == document.getElementById('user-field').value && user.password == document.getElementById('password-field').value) {
-        console.log('Login bem sucedido!')
-        return true;
+    let users = buscarUsuarios();
+
+    let inputUsername = document.getElementById('user-field').value;
+    let inputPassword = document.getElementById('password-field').value;
+
+    if (validarFiltroUsuario(users, inputUsername) != 0) {
+        let user = validarFiltroUsuario(users, inputUsername);
+        if (user[0].username == inputUsername && user[0].password == inputPassword) {
+            registrarUsuarioLogado(user[0]);
+            return true;
+        }
+        exibirMensagem('Senha incorreta.');
+        return false;
     } else {
-        console.log('Erro no login')
-        exibirMensagem();
+        console.log('Erro no login: usuário não encontrado.');
+        exibirMensagem('Usuário não encontrado.');
         return false
     }
 }
 
-const exibirMensagem = () => {
-    document.getElementById('error-message').style = 'display: block;'
-
+const validarFiltroUsuario = (jsons, inputUsername) => {
+    let checkArray = jsons.filter(json => json.username == inputUsername);
+    return checkArray
 }
 
-const retirarMensagem = () => { 
+const registrarUsuarioLogado = (usuarioValidado) => {
+    let usuarioLogado = usuarioValidado;
+    sessionStorage.setItem('usuarioLogado', JSON.stringify(usuarioLogado))
+}
+
+const exibirMensagem = (mensagem) => {
+    let caixaMensagem = document.getElementById('error-message');
+    caixaMensagem.style = 'display: block;';
+    caixaMensagem.innerText = mensagem;
+}
+
+const retirarMensagem = () => {
     document.getElementById('error-message').style = 'display: none;'
 }
 
 
 const criarUsuarioTest = () => {
-    let user = { user: 'guilhermecastello', password: '123' }
-    localStorage.setItem('user', JSON.stringify(user))
+    let users = [
+        { "username": "guilhermecastello", "nome": "Guilherme", "sobrenome": "Castello", "password": "123", "url": "/images/2.jpg", "cargo": "Super Admin" },
+        { "username": "emilyprocopio", "nome": "Emily", "sobrenome": "Procopio", "password": "123", "url": "/images/nic.jpeg", "cargo": "Super Admin" },
+        { "username": "teste", "nome": "Teste", "sobrenome": "Teste", "password": "123", "url": "", "cargo": "Teste" }
+    ]
+    localStorage.setItem('users', JSON.stringify(users));
 }
 
 criarUsuarioTest();
